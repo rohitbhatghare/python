2cbf723f6b1614e96a9890d8b0dfa1a44b0309fe
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-10-15 14:00:10 +0530

    Palindrome program

---
+++ Oct-15-10-2020/palindrome.py
@@ -0,0 +1,21 @@
+
+a=int(input("Enter the three digit number"))
+
+num=a
+a1=int(num%10)
+num=int(num/10)
+a2=int(num%10)
+num=int(num/10)
+a3=int(num%10)
+num=int(num/10)
+
+rev=a1*100+a2*10+a3
+
+print (rev)
+
+if a==rev :
+    print("palindrome")
+else :
+    print("Not palindrome")
+
+
