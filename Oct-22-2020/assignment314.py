85792a56b0879405c255239ea788bc26b172f2e3
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-10-27 12:23:49 +0530

    Patterns Programs

---
+++ Oct-22-2020/assignment314.py
@@ -0,0 +1,8 @@
+dec=1
+for x in range(5,0,-1):
+    for y in range(x,0,-1):
+        print(" ",end="")
+    for z in range(dec,0,-1):
+        print(chr(z+64),end="")
+    dec += 2
+    print(" ")
---
+++ Oct-22-2020/assignment320.py
@@ -0,0 +1,6 @@
+dec=7
+for x in range (4,0,-1):
+    for y in range(x,5):
+        print("  ",end=" ")
+        print("*"*dec)
+        dec-=2
