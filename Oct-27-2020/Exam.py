c794ae183810fcda6d7b272c12b2f2feb7a2fbce
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-10-27 14:43:31 +0530

    Pattern Programs

---
+++ Oct-27-2020/Exam.py
@@ -0,0 +1,16 @@
+#Which of the following expression involves corcion when evaluated in python?
+a=1.7%2
+print(a)
+
+#What is result of cmp (3,1)?
+#a=cmp(3,1)
+
+#print(a)
+import sys
+l1=tuple()
+print(sys.getsizeof(l1),end="\n")
+
+d1=[10,20,30,40,50]
+d2=[1,2,3,4,5]
+print(d1-d2)
+
