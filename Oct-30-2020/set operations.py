61aa854c1c1f573cb816877d1dd009a4999e71ff
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-10-30 12:14:48 +0530

    Programs set function

---
+++ Oct-30-2020/set operations.py
@@ -0,0 +1,13 @@
+s={10,20,30,40}
+print(s)
+l=[1,2,3,4,10,20,40,50]
+print(l)
+s.update(l,range(6))
+print(s)
+
+s={10,20,30,40}
+print(s)
+l=[1,2,3,4,10,20,40,50]
+print(l)
+s.update(l,range(6))
+s.remove(l)
