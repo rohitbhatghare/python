d2c310cb86c2b70dba8f747556562e7208a82787
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-11-03 14:29:43 +0530

    Pattern Programs

---
+++ Nov-3-2020/assignment352.py
@@ -0,0 +1,13 @@
+n=5
+wx=n
+wy=n
+
+for x in range(1,n+1):
+    for y in range(1,n*2+1):
+        if(y==wx or y==wy):
+            print(wx,end="")
+        else:
+            print(" ",end="")
+    wx-=1
+    wy+=1
+    print()
