ead5d5b270dd96f0dc7a9ae21fbec0e5d997eca3
rohitbhatghare <70204003+rohitbhatghare@users.noreply.github.com>
2020-11-03 14:47:25 +0530

    Patterns Programs

---
+++ Nov-3-2020/assignment353.py
@@ -0,0 +1,13 @@
+n=5
+wx=n
+wy=n
+
+for x in range(1,n+1):
+    for y in range(1,n*2+1):
+        if(y==wx or y==wy):
+            print(chr(x+64),end="")
+        else:
+            print(" ",end="")
+    wx-=1
+    wy+=1
+    print()
